a5161f5e7494ef1397b47fcc25fbd47640a38953
Mudassir03 <118797440+Mudassir03@users.noreply.github.com>
2023-02-23 15:31:11 +0530

    done

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+import "./Slider.scss";
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const data = [
+    "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  ];
+
+ 
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : currentSlide+ 1);
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      nextSlide();
+    }, 5000);
+  });
+
+  return (
+    <div className="slider">
+      <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
+      </div>
+    
+    </div>
+  );
+};
+
+export default Slider;
